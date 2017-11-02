976fa7d1d74756c6d08874a601d12301757c49de
Fomichev Alexandr <kolegov139@gmail.com>
2017-11-02 13:54:11 +0300

    deleted useless strings and fuctions in main

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 class Base
 {
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
+public:      
+    Base() {cout << "Construct Base object\n"; }
+    ~Base() {cout << "Destruct Base object\n"; }
 
     void func() { cout << "Function func() of class Base\n"; }
 };
@@ -18,19 +18,10 @@ public:
     ~Child() { cout << "Destruct Child object\n"; }
 };
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "* Create Object base" << endl;
     Base base;
-
-    cout << "* Create Object child" << endl;
     Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    base.func();
-
-    cout << "* Call method func() for object child" << endl;
-    child.func();
 
     return 0;
 }
